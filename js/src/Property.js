e65dcbae8ca37dcae442a41649783c90232caddf
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-06-17 01:11:14 -0700

    06/17/2016 1:11 AM

--- js/src/Property.js
+++ js/src/Property.js
@@ -1,12 +1,8 @@
-var Any, Kind, LazyProperty, LazyVar, NamedFunction, Property, ProxyProperty, PureObject, ReactiveProperty, SimpleProperty, Tracer, Void, assert, assertType, assertTypes, configTypes, define, emptyFunction, guard, internalPrototype, isProto, isType, key, prototype, setType, throwFailure, value;
+var Any, Kind, NamedFunction, Property, PropertyInternal, PureObject, Tracer, Void, assertType, assertTypes, configTypes, define, isProto, isType, setType;
 
 require("isDev");
 
-throwFailure = require("failure").throwFailure;
-
 NamedFunction = require("NamedFunction");
-
-emptyFunction = require("emptyFunction");
 
 assertTypes = require("assertTypes");
 
@@ -22,288 +18,86 @@ Tracer = require("tracer");
 
 isType = require("isType");
 
-assert = require("assert");
-
-guard = require("guard");
-
 Void = require("Void");
 
 Kind = require("Kind");
 
 Any = require("Any");
 
-ReactiveProperty = require("./ReactiveProperty");
-
-SimpleProperty = require("./SimpleProperty");
-
-ProxyProperty = require("./ProxyProperty");
-
-LazyProperty = require("./LazyProperty");
-
-LazyVar = require("./inject/LazyVar");
+PropertyInternal = require("./PropertyInternal");
 
 define = Object.defineProperty;
 
-if (isDev) {
-  configTypes = {
-    value: Any,
-    needsValue: [Boolean, Void],
-    frozen: [Boolean, Void],
-    writable: [Boolean, Void],
-    configurable: [Boolean, Void],
-    enumerable: [Boolean, Void],
-    get: [Function, Void],
-    set: [Function, Void],
-    didSet: [Function, Void],
-    willSet: [Function, Void],
-    lazy: [Function, Void],
-    reactive: [Boolean, Void]
-  };
-}
+isDev && (configTypes = {
+  value: Any,
+  needsValue: [Boolean, Void],
+  frozen: [Boolean, Void],
+  writable: [Boolean, Void],
+  configurable: [Boolean, Void],
+  enumerable: [Boolean, Void],
+  get: [Function, Void],
+  set: [Function, Void],
+  didSet: [Function, Void],
+  willSet: [Function, Void],
+  lazy: [Function, Void],
+  reactive: [Boolean, Void]
+});
 
-module.exports = Property = NamedFunction("Property", function(config) {
+Property = NamedFunction("Property", function(config) {
   var self;
-  if (!config) {
+  if (config == null) {
     config = {};
   }
-  if (isDev) {
-    assertTypes(config, configTypes);
-  }
+  isDev && assertTypes(config, configTypes);
   self = {
     simple: true,
     writable: true,
     configurable: true
   };
-  if (isDev) {
-    define(self, "_tracer", {
-      value: Tracer("Property()")
-    });
-  }
+  isDev && define(self, "_tracer", {
+    value: Tracer("Property()")
+  });
   setType(self, Property);
   self._parseConfig(config);
   return self;
 });
 
+PropertyInternal.define(Property.prototype);
+
+module.exports = Property;
+
 Property.targetType = [Kind(Object), PureObject];
 
-Property.keyType = global.Symbol ? [String, Symbol] : [String];
+Property.keyType = global.Symbol ? [String, Symbol] : String;
 
-prototype = {
-  define: function(target, key, value) {
-    var enumerable;
-    assertType(target, Property.targetType);
-    assertType(key, Property.keyType);
-    if (arguments.length === 2) {
-      value = this.value;
-    }
-    if (this.needsValue) {
-      if (value === void 0) {
-        return;
-      }
-    }
-    enumerable = isDev ? this._isEnumerable(key) : true;
-    if (this.simple && enumerable) {
-      target[key] = value;
-    } else {
-      this._define(target, key, value, enumerable);
-    }
+Property.mutable = Property();
+
+Property.frozen = Property({
+  frozen: true
+});
+
+Property.hidden = Property({
+  enumerable: false
+});
+
+Property.prototype.define = function(target, key) {
+  var enumerable, value;
+  assertType(target, Property.targetType);
+  assertType(key, Property.keyType);
+  if (arguments.length > 2) {
+    value = arguments[2];
+  } else {
+    value = this.value;
   }
-};
-
-internalPrototype = {
-  _isEnumerable: function(key) {
-    if (isType(key, Symbol)) {
-      return true;
-    }
-    if (this.enumerable !== void 0) {
-      return this.enumerable;
-    }
-    return key[0] !== "_";
-  },
-  _define: function(target, key, value, enumerable) {
-    var descriptor, get, prop;
-    if (isProto(target)) {
-      if (this.get) {
-        descriptor = {
-          get: this.get,
-          set: this.set,
-          enumerable: enumerable,
-          configurable: this.configurable
-        };
-      } else if (this.lazy) {
-        descriptor = {
-          get: LazyVar(this.lazy).get,
-          set: function() {
-            throw Error("'" + key.toString() + "' is not writable.");
-          },
-          enumerable: enumerable,
-          configurable: this.configurable
-        };
-      } else {
-        descriptor = {
-          value: value,
-          enumerable: enumerable,
-          writable: this.writable,
-          configurable: this.configurable
-        };
-      }
-    } else {
-      value = this.transformValue(value);
-      get = this.createGetter(value);
-      descriptor = {
-        get: get,
-        set: this.createSetter(key, value, get),
-        enumerable: enumerable,
-        configurable: this.configurable
-      };
-    }
-    prop = this;
-    return guard(function() {
-      return define(target, key, descriptor);
-    }).fail(function(error) {
-      return throwFailure(error, {
-        target: target,
-        key: key,
-        value: value,
-        descriptor: descriptor,
-        prop: prop,
-        stack: isDev ? prop._tracer() : void 0
-      });
-    });
-  },
-  _parseConfig: function(config) {
-    var type;
-    if (isDev) {
-      this._parseAttributes(config);
-    }
-    type = this._parseType(config);
-    if (this.simple && !isDev) {
-      return;
-    }
-    this.get = config.get;
-    this.set = config.set;
-    this.lazy = config.lazy;
-    this.transformValue = type.transformValue(config);
-    this.createGetter = type.createGetter;
-    return this.createSetter = this._createSetterType(type, config);
-  },
-  _parseAttributes: function(config) {
-    this.enumerable = config.enumerable;
-    if (config.frozen) {
-      this.simple = false;
-      this.writable = false;
-      this.configurable = false;
-      return;
-    }
-    if (config.writable === false) {
-      this.simple = false;
-      this.writable = false;
-    }
-    if (config.configurable === false) {
-      this.simple = false;
-      return this.configurable = false;
-    }
-  },
-  _parseType: function(config) {
-    if (config.get) {
-      this.simple = false;
-      if (!config.set) {
-        this.writable = false;
-      }
-      return ProxyProperty;
-    }
-    assert(!config.set, {
-      reason: "Cannot define 'set' without 'get'!"
-    });
-    if (config.lazy) {
-      this.simple = false;
-      return LazyProperty;
-    }
-    this.value = config.value;
-    if (config.needsValue === true) {
-      this.needsValue = true;
-    }
-    if (config.reactive) {
-      this.simple = false;
-      return ReactiveProperty;
-    }
-    return SimpleProperty;
-  },
-  _createSetterType: function(type, config) {
-    if (this.writable) {
-      return this._wrapSetterType(type.createSetter, config.willSet, config.didSet);
-    }
-    if (!isDev) {
-      return function() {
-        return emptyFunction;
-      };
-    }
-    return function(key) {
-      return function() {
-        throw Error("'" + key.toString() + "' is not writable.");
-      };
-    };
-  },
-  _wrapSetterType: function(createSetter, willSet, didSet) {
-    var wrapSetter;
-    wrapSetter = emptyFunction.thatReturnsArgument;
-    if (willSet) {
-      if (didSet) {
-        this.simple = false;
-        wrapSetter = function(setter) {
-          return function(newValue, oldValue) {
-            newValue = willSet.call(this, newValue, oldValue);
-            setter.call(this, newValue, oldValue);
-            return didSet.call(this, newValue, oldValue);
-          };
-        };
-      } else {
-        this.simple = false;
-        wrapSetter = function(setter) {
-          return function(newValue, oldValue) {
-            newValue = willSet.call(this, newValue, oldValue);
-            return setter.call(this, newValue, oldValue);
-          };
-        };
-      }
-    } else if (didSet) {
-      this.simple = false;
-      wrapSetter = function(setter) {
-        return function(newValue, oldValue) {
-          setter.call(this, newValue, oldValue);
-          return didSet.call(this, newValue, oldValue);
-        };
-      };
-    }
-    return function(key, value, get) {
-      var getter, setter;
-      setter = wrapSetter(createSetter(value));
-      if (setter.length < 2) {
-        return function(newValue) {
-          return setter.call(this, newValue);
-        };
-      }
-      getter = get.safely || get;
-      return function(newValue) {
-        return setter.call(this, newValue, getter.call(this));
-      };
-    };
+  if (this.needsValue && (value === void 0)) {
+    return;
   }
+  enumerable = isDev ? this._isEnumerable(key) : true;
+  if (this.simple && enumerable) {
+    target[key] = value;
+    return;
+  }
+  this._define(target, key, value, enumerable);
 };
-
-for (key in prototype) {
-  value = prototype[key];
-  define(Property.prototype, key, {
-    value: value,
-    enumerable: true
-  });
-}
-
-for (key in internalPrototype) {
-  value = internalPrototype[key];
-  define(Property.prototype, key, {
-    value: value
-  });
-}
 
 //# sourceMappingURL=../../map/src/Property.map
--- src/Property.coffee
+++ src/Property.coffee
@@ -1,10 +1,7 @@
 
 require "isDev"
 
-{ throwFailure } = require "failure"
-
 NamedFunction = require "NamedFunction"
-emptyFunction = require "emptyFunction"
 assertTypes = require "assertTypes"
 assertType = require "assertType"
 PureObject = require "PureObject"
@@ -12,50 +9,39 @@ setType = require "setType"
 isProto = require "isProto"
 Tracer = require "tracer"
 isType = require "isType"
-assert = require "assert"
-guard = require "guard"
 Void = require "Void"
 Kind = require "Kind"
 Any = require "Any"
 
-ReactiveProperty = require "./ReactiveProperty"
-SimpleProperty = require "./SimpleProperty"
-ProxyProperty = require "./ProxyProperty"
-LazyProperty = require "./LazyProperty"
-LazyVar = require "./inject/LazyVar"
+PropertyInternal = require "./PropertyInternal"
 
 define = Object.defineProperty
 
-if isDev
-  configTypes =
-    value: Any
-    needsValue: [ Boolean, Void ]
-    frozen: [ Boolean, Void ]
-    writable: [ Boolean, Void ]
-    configurable: [ Boolean, Void ]
-    enumerable: [ Boolean, Void ]
-    get: [ Function, Void ]
-    set: [ Function, Void ]
-    didSet: [ Function, Void ]
-    willSet: [ Function, Void ]
-    lazy: [ Function, Void ]
-    reactive: [ Boolean, Void ]
+isDev and configTypes =
+  value: Any
+  needsValue: [ Boolean, Void ]
+  frozen: [ Boolean, Void ]
+  writable: [ Boolean, Void ]
+  configurable: [ Boolean, Void ]
+  enumerable: [ Boolean, Void ]
+  get: [ Function, Void ]
+  set: [ Function, Void ]
+  didSet: [ Function, Void ]
+  willSet: [ Function, Void ]
+  lazy: [ Function, Void ]
+  reactive: [ Boolean, Void ]
 
-module.exports =
-Property = NamedFunction "Property", (config) ->
+Property = NamedFunction "Property", (config = {}) ->
 
-  config = {} unless config
-
-  assertTypes config, configTypes if isDev
+  isDev and assertTypes config, configTypes
 
   self =
     simple: yes
     writable: yes
     configurable: yes
 
-  if isDev
-    define self, "_tracer",
-      value: Tracer "Property()"
+  isDev and define self, "_tracer",
+    value: Tracer "Property()"
 
   setType self, Property
 
@@ -63,198 +49,50 @@ Property = NamedFunction "Property", (config) ->
 
   return self
 
-Property.targetType = [ Kind(Object), PureObject ]
-Property.keyType = if global.Symbol then [ String, Symbol ] else [ String ]
+PropertyInternal.define Property.prototype
 
-prototype =
+module.exports = Property
 
-  define: (target, key, value) ->
+#
+# Class properties
+#
 
-    assertType target, Property.targetType
-    assertType key, Property.keyType
+Property.targetType = [
+  Kind Object
+  PureObject
+]
 
-    if arguments.length is 2
-      value = @value
+Property.keyType =
+  if global.Symbol
+    [ String, Symbol ]
+  else String
 
-    if @needsValue
-      return if value is undefined
+Property.mutable = Property()
 
-    enumerable = if isDev then @_isEnumerable key else yes
+Property.frozen = Property { frozen: yes }
 
-    if @simple and enumerable
-      target[key] = value
-    else @_define target, key, value, enumerable
+Property.hidden = Property { enumerable: no }
+
+#
+# Public methods
+#
+
+Property.prototype.define = (target, key) ->
+
+  assertType target, Property.targetType
+  assertType key, Property.keyType
+
+  if arguments.length > 2
+    value = arguments[2]
+  else value = @value
+
+  return if @needsValue and (value is undefined)
+
+  enumerable = if isDev then @_isEnumerable key else yes
+
+  if @simple and enumerable
+    target[key] = value
     return
 
-internalPrototype =
-
-  _isEnumerable: (key) ->
-    return yes if isType key, Symbol
-    return @enumerable if @enumerable isnt undefined
-    return key[0] isnt "_"
-
-  _define: (target, key, value, enumerable) ->
-
-    if isProto target
-
-      if @get
-        descriptor = {
-          @get
-          @set
-          enumerable
-          @configurable
-        }
-
-      else if @lazy
-        descriptor = {
-          get: LazyVar(@lazy).get
-          set: -> throw Error "'" + key.toString() + "' is not writable."
-          enumerable
-          @configurable
-        }
-
-      else
-        descriptor = {
-          value
-          enumerable
-          @writable
-          @configurable
-        }
-
-    else
-      value = @transformValue value
-      get = @createGetter value
-      descriptor = {
-        get
-        set: @createSetter key, value, get
-        enumerable
-        @configurable
-      }
-
-    prop = this
-    guard -> define target, key, descriptor
-    .fail (error) -> throwFailure error, {
-      target, key, value, descriptor, prop
-      stack: prop._tracer() if isDev
-    }
-
-  _parseConfig: (config) ->
-
-    # We force 'enumerable', 'configurable', and 'writable'
-    # to be true when not in __DEV__ mode so we can avoid
-    # any unnecessary calls to 'Object.defineProperty'.
-    @_parseAttributes config if isDev
-
-    type = @_parseType config
-
-    # When not in __DEV__ mode, we can be absolutely certain
-    # that 'enumerable' equals true. With that in mind, we can avoid the
-    # cost of creating 'transformValue', 'createGetter', and 'createSetter'!
-    return if @simple and not isDev
-
-    @get = config.get
-    @set = config.set
-    @lazy = config.lazy
-
-    @transformValue = type.transformValue config
-    @createGetter = type.createGetter
-    @createSetter = @_createSetterType type, config
-
-  _parseAttributes: (config) ->
-
-    @enumerable = config.enumerable
-
-    if config.frozen
-      @simple = no
-      @writable = no
-      @configurable = no
-      return
-
-    if config.writable is no
-      @simple = no
-      @writable = no
-
-    if config.configurable is no
-      @simple = no
-      @configurable = no
-
-  _parseType: (config) ->
-
-    if config.get
-      @simple = no
-      @writable = no unless config.set
-      return ProxyProperty
-
-    assert not config.set,
-      reason: "Cannot define 'set' without 'get'!"
-
-    if config.lazy
-      @simple = no
-      return LazyProperty
-
-    @value = config.value
-
-    if config.needsValue is yes
-      @needsValue = yes
-
-    if config.reactive
-      @simple = no
-      return ReactiveProperty
-
-    return SimpleProperty
-
-  _createSetterType: (type, config) ->
-
-    if @writable
-      return @_wrapSetterType type.createSetter, config.willSet, config.didSet
-
-    unless isDev
-      return -> emptyFunction
-
-    return (key) ->
-      return -> throw Error "'" + key.toString() + "' is not writable."
-
-  _wrapSetterType: (createSetter, willSet, didSet) ->
-
-    wrapSetter = emptyFunction.thatReturnsArgument
-
-    if willSet
-
-      if didSet
-        @simple = no
-        wrapSetter = (setter) ->
-          return (newValue, oldValue) ->
-            newValue = willSet.call this, newValue, oldValue
-            setter.call this, newValue, oldValue
-            didSet.call this, newValue, oldValue
-
-      else
-        @simple = no
-        wrapSetter = (setter) ->
-          return (newValue, oldValue) ->
-            newValue = willSet.call this, newValue, oldValue
-            setter.call this, newValue, oldValue
-
-    else if didSet
-      @simple = no
-      wrapSetter = (setter) ->
-        return (newValue, oldValue) ->
-          setter.call this, newValue, oldValue
-          didSet.call this, newValue, oldValue
-
-    return (key, value, get) ->
-
-      setter = wrapSetter createSetter value
-      if setter.length < 2
-        return (newValue) ->
-          setter.call this, newValue
-
-      getter = get.safely or get
-      return (newValue) ->
-        setter.call this, newValue, getter.call this
-
-for key, value of prototype
-  define Property.prototype, key, { value, enumerable: yes }
-
-for key, value of internalPrototype
-  define Property.prototype, key, { value }
+  @_define target, key, value, enumerable
+  return
